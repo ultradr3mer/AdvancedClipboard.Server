274fd50b0909586f8b5258689e786744ccc67c85
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2021-03-03 21:28:11 +0100

    ClipboardGetData fixed

--- AdvancedClipboard.Server/Data/ClipboardGetData.cs
+++ AdvancedClipboard.Server/Data/ClipboardGetData.cs
@@ -5,6 +5,6 @@ namespace AdvancedClipboard.Server.Data
   public class ClipboardGetData
   {
     public Guid Id { get; internal set; }
-    public object PlainTextContent { get; internal set; }
+    public string PlainTextContent { get; internal set; }
   }
 }
